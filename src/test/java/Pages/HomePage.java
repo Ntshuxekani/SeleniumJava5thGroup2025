d45aacd93aa1c107e22600f529f79dbc1b2ffcf9
Ntshuxekani <ntshuxekanizitha@gmail.com>
2025-10-21 18:13:03 +0200

    added HomePage

---
+++ src/test/java/Pages/HomePage.java
@@ -0,0 +1,29 @@
+package Pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class HomePage {
+
+    WebDriver driver;
+
+    @FindBy(id = "overview-hero")
+    WebElement homepageTitle_id;
+
+    @FindBy(id = "nav-btn-practice")
+    WebElement LearningMaterial_id;
+
+    public HomePage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void verifyHomePageIsDisplayed() {
+        homepageTitle_id.isDisplayed();
+    }
+
+    public void clickLearningMaterial() {
+        LearningMaterial_id.click();
+
+    }
+}
